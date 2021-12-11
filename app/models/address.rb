10f24e9894257651595169e58c375967db284ccc
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2021-12-11 17:56:08 +0100

    Add Address validations.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :dealer
+
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :zipcode, presence: true
+  validates :country, presence: true
+  validates :latitude, presence: true, numericality: true
+  validates :longitude, presence: true, numericality: true
 end

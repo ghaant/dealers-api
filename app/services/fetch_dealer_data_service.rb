d9a05b86b631c440bd9a437cb4cd9551103c9d87
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2021-12-11 16:55:04 +0100

    Create FetchDealerDataService.

---
+++ app/services/fetch_dealer_data_service.rb
@@ -0,0 +1,9 @@
+require 'faraday'
+
+class FetchDealerDataService
+  FAKER_API_ENDPOINT = 'https://fakerapi.it/api/v1/companies?_seed=1&_quantity=200'.freeze
+
+  def self.call
+    Faraday.get(FAKER_API_ENDPOINT)
+  end
+end

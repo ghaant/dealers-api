7595364c2a3c2b6dc64234b9f556cbfa9d19666a
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2021-12-12 14:09:36 +0100

    A route for syncing the data.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get '/sync_data', to: 'dealers#sync_data'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

e30fcb67234da08c90e2769fd575ce013feba4d8
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2021-12-12 15:36:02 +0100

    A route for listing the dealers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/sync_data', to: 'dealers#sync_data'
+  get '/index', to: 'dealers#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

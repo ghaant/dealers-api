33eed686c66ef6262222850f5960b19fd5218c45
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2021-12-12 17:10:05 +0100

    Make the search case insensitive.

--- app/services/find_dealers_service.rb
+++ app/services/find_dealers_service.rb
@@ -1,23 +1,23 @@
 class FindDealersService
   def initialize(search_params)
-    @name = search_params[:name]
+    @name = search_params[:name].downcase
     @phone = search_params[:phone]
-    @street = search_params[:street]
-    @city = search_params[:city]
-    @zipcode = search_params[:zipcode]
-    @country = search_params[:country]
+    @street = search_params[:street].downcase
+    @city = search_params[:city].downcase
+    @zipcode = search_params[:zipcode].downcase
+    @country = search_params[:country].downcase
   end
 
   def call
     Dealer.joins(:addresses).merge(Address.current_addresses).
       where(
         "
-          dealers.name LIKE ?
+          LOWER(dealers.name) LIKE ?
           AND CAST(dealers.phone AS VARCHAR) LIKE ?
-          AND addresses.street LIKE ?
-          AND addresses.city = COALESCE(?, addresses.city)
-          AND addresses.zipcode = COALESCE(?, addresses.zipcode)
-          AND addresses.country = COALESCE(?, addresses.country)
+          AND LOWER(addresses.street) LIKE ?
+          AND LOWER(addresses.city) = COALESCE(?, addresses.city)
+          AND LOWER(addresses.zipcode) = COALESCE(?, addresses.zipcode)
+          AND LOWER(addresses.country) = COALESCE(?, addresses.country)
         ",
         "%#{@name}%", "%#{@phone}%", "%#{@street}%", @city, @zipcode, @country
       )

c27aa2fe030651b22335251ea11c4b7ccf7cf33a
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2021-12-12 14:43:47 +0100

    Return 200 fro,sync_data controller method.

--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -2,6 +2,8 @@ class DealersController < ApplicationController
   def sync_data
     response = FetchDealerDataService.call
 
-    SyncDealerDataService.new(response).call if response&.status == 200
+    if response&.status == 200 && SyncDealerDataService.new(response).call
+      render json: 'The dealer data has been successfully synced.', status: 200
+    end
   end
 end

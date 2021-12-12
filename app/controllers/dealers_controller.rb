5deae42270f44bb6a453ef170c2a2dc04d84e83a
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2021-12-12 17:35:21 +0100

    Working index endpoint.

--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -6,4 +6,37 @@ class DealersController < ApplicationController
       render json: 'The dealer data has been successfully synced.', status: 200
     end
   end
+
+  def index
+    dealers = FindDealersService.new(search_params).call
+
+    render json: format_dealers_output(dealers)
+  end
+
+  private
+
+  def search_params
+    {
+      name: params[:name],
+      phone: params[:phone],
+      street: params[:street],
+      city: params[:city],
+      zipcode: params[:zipcode],
+      country: params[:country]
+    }
+  end
+
+  def format_dealers_output(dealers)
+    dealers.select('
+      dealers.id,
+      dealers.name,
+      dealers.phone,
+      addresses.street,
+      addresses.city,
+      addresses.zipcode,
+      addresses.country,
+      addresses.latitude,
+      addresses.longitude
+      ')
+  end
 end
--- app/services/find_dealers_service.rb
+++ app/services/find_dealers_service.rb
@@ -1,11 +1,11 @@
 class FindDealersService
   def initialize(search_params)
-    @name = search_params[:name].downcase
+    @name = search_params[:name]&.downcase
     @phone = search_params[:phone]
-    @street = search_params[:street].downcase
-    @city = search_params[:city].downcase
-    @zipcode = search_params[:zipcode].downcase
-    @country = search_params[:country].downcase
+    @street = search_params[:street]&.downcase
+    @city = search_params[:city]
+    @zipcode = search_params[:zipcode]
+    @country = search_params[:country]
   end
 
   def call
@@ -15,9 +15,9 @@ class FindDealersService
           LOWER(dealers.name) LIKE ?
           AND CAST(dealers.phone AS VARCHAR) LIKE ?
           AND LOWER(addresses.street) LIKE ?
-          AND LOWER(addresses.city) = COALESCE(?, addresses.city)
-          AND LOWER(addresses.zipcode) = COALESCE(?, addresses.zipcode)
-          AND LOWER(addresses.country) = COALESCE(?, addresses.country)
+          AND LOWER(addresses.city) = LOWER(COALESCE(?, addresses.city))
+          AND LOWER(addresses.zipcode) = LOWER(COALESCE(?, addresses.zipcode))
+          AND LOWER(addresses.country) = LOWER(COALESCE(?, addresses.country))
         ",
         "%#{@name}%", "%#{@phone}%", "%#{@street}%", @city, @zipcode, @country
       )
